4a6bdc306dd61d084f19d9ab3187526598ad16f7
KarlyGay <77978213+KarlyGay@users.noreply.github.com>
2021-01-28 19:24:16 -0700

    Update Main.java
    Adding the line to the right place this time.

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jair Valadez");
-	
+	System.out.println("Hello Jair, my name is Karly");	
     }
 }

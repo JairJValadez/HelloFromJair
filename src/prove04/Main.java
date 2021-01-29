ca408e25db970d161c16f114ddb83f19fb12b748
Hyrum Elwood <elwo19002@byui.edu>
2021-01-28 20:36:55 -0700

    Peer Update

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jair Valadez");
-	System.out.println("Hello Jair, my name is Karly");	
+	    System.out.println("Hello Jair, my name is Karly");
+        System.out.println("Hello Jair, my name is Hyrum Elwood");
+
     }
 }
